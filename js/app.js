93617e029edc7db4f60f4de7f365113aa6b8e868
Harihar Subramanyam <hahasubbu@gmail.com>
2015-05-11 19:31:25 -0400

    Cards

--- js/app.js
+++ js/app.js
@@ -2,9 +2,19 @@ angular.module("snapSim", [])
   .controller("appController", function($scope) {
     // Set up some constants.
     var DATA_URL = "data/story_cart.json";
+    var BUDGET = 10.00;
 
     // Make scope accessible globally for console debugging.
     window.$s = $scope;
+
+    // Set the current page.
+    $scope.pageNumber = 2;
+
+    // Increment the page.
+    $scope.advancePage = function() {
+      $scope.pageNumber++;
+      $scope.$apply();
+    };
 
     // Get data.
     $.getJSON(DATA_URL, function(data) {
@@ -12,5 +22,7 @@ angular.module("snapSim", [])
       console.log(data);
       $scope.$apply();
     });
+
+    $scope.moneyLeft = BUDGET;
 
   });
