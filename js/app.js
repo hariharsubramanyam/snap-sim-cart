aee2d6bba09b9c0fa080bad8d407615d1c2b9cd0
Harihar Subramanyam <hahasubbu@gmail.com>
2015-05-11 19:58:29 -0400

    Change budget as items are picked

--- js/app.js
+++ js/app.js
@@ -2,27 +2,45 @@ angular.module("snapSim", [])
   .controller("appController", function($scope) {
     // Set up some constants.
     var DATA_URL = "data/story_cart.json";
-    var BUDGET = 10.00;
+    var BUDGET = 20.00;
 
     // Make scope accessible globally for console debugging.
     window.$s = $scope;
 
     // Set the current page.
-    $scope.pageNumber = 2;
+    $scope.pageNumber = 1;
 
     // Increment the page.
     $scope.advancePage = function() {
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
       $scope.pageNumber++;
-      $scope.$apply();
     };
+
+    $scope.budget = BUDGET;
+    $scope.used = BUDGET;
 
     // Get data.
     $.getJSON(DATA_URL, function(data) {
       $scope.story = data;
-      console.log(data);
-      $scope.$apply();
+      var used = 0;
+      data.items.forEach(function(item) {
+        item.selected = true;
+        used += item.price;
+      });
+      $scope.used = round(used);
     });
+    
+    var round = function(x) {
+      return Math.round(x * 100.0) / 100.0;
+    };
 
-    $scope.moneyLeft = BUDGET;
-
+    $scope.itemSelected = function(item) {
+      item.selected = !item.selected;
+      var multiplier = -1;
+      if (item.selected) {
+        multiplier = 1;
+      }
+      $scope.used += multiplier * item.price;
+      $scope.used = round($scope.used);
+    };
   });
